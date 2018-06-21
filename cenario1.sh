2ae15471c3b0a70c861f7ef3f191ac3ac6e7bfe1
Hugo Dantas <hugobcmt@gmail.com>
2018-06-21 12:17:49 -0300

    codigo pronto para testar o cenario 1

---
+++ cenario1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python grupo2.py -c 10 -h 54.85.161.250 -p 3421 -b 1024 -t 1
--- grupo2.py
+++ grupo2.py
@@ -4,8 +4,8 @@
 # Testa a vazão da rede
 #
 # Usage:
-# 1) on host_A: throughput -s [port]                                                # start a server
-# 2) on host_B: throughput -c  count -h host_A -p [port] [-b bufsize] [-t tempo]      # start a client
+# 1) on host_A: python grupo2.py -s [port]                                                  # start a server
+# 2) on host_B: python grupo2.py -c  count -h host_A -p [port] [-b bufsize] [-t tempo]      # start a client
 #
 # O servidor irá servir múltiplos clientes até ser desligado
 #
@@ -17,6 +17,7 @@
 
 import sys, time
 from socket import *
+from math import sqrt
 
 MY_PORT = 50000 + 42
 
@@ -36,8 +37,8 @@ def main():
 
 def usage():
     sys.stdout = sys.stderr
-    print 'Usage:    (on host_A) throughput -s [port]'
-    print 'and then: (on host_B) throughput -c  count -h host_A -p [port] [-b bufsize] [-t seconds]'
+    print 'Usage:    (on host_A) python grupo2.py -s [port]'
+    print 'and then: (on host_B) python grupo2.py -c  count -h host_A -p [port] [-b bufsize] [-t seconds]'
     sys.exit(2)
 
 
@@ -63,12 +64,32 @@ def server():
         conn.close()
         print 'Done with', host, 'port', remoteport
 
+def media(dataset): #calcula media dos dados
+	soma=0.0
+	for i in range(0, len(dataset)):
+		soma=soma+dataset[i]
+	
+	return soma/len(dataset)
+
+def desvioPadrao(dataset): #desvio padrao 
+	media_local=0.0
+	media_local=media(dataset)
+	soma=0
+
+	for i in range(0, len(dataset)):
+		soma=(soma + ( (dataset[i] - media_local) * (dataset[i] - media_local) ) )
+
+	variance=(soma/( len(dataset) - 1.0) )
+	
+	return sqrt(variance)
 
 def client():
 
     global BUFSIZE
 
     tempo = 0
+
+    vazoes = []
 
     if ('-c' not in sys.argv) or ('-h' not in sys.argv) or ('-p' not in sys.argv):
         usage()
@@ -98,28 +119,39 @@ def client():
     testdata = 'x' * (BUFSIZE-1) + '\n'
     t1 = time.time()
     s = socket(AF_INET, SOCK_STREAM)
-    t2 = time.time()
+    #t2 = time.time()
     s.connect((host, port))
-    t3 = time.time()
+    #t3 = time.time()
 
-    for i in range(count):
-        i = i+1
-        s.send(testdata)
-        time.sleep(tempo)
+    for t in range(10):
 
-    s.shutdown(1) # Send EOF
-    t4 = time.time()
-    data = s.recv(BUFSIZE)
-    t5 = time.time()
+        for i in range(count):
+            i = i+1
+            s.send(testdata)
+            time.sleep(tempo)
 
-    print data
+        s.shutdown(1) # Send EOF
+        #t4 = time.time()
+        data = s.recv(BUFSIZE)
+        t5 = time.time()
+
+        vazao = round((BUFSIZE*count*0.001) / (t5-t1), 3)
+        vazoes.append(vazao)
+
     print 'Tamanho do pacote:', BUFSIZE
     print '\nTempo entre geração de pacotes (segundos):', tempo
-    print '\nRaw timers:', t1, t2, t3, t4, t5
-    print '\nIntervalos:', t2-t1, t3-t2, t4-t3, t5-t4
-    print '\nTotal:', t5-t1
-    print '\nThroughput:', round((BUFSIZE*count*0.001) / (t5-t1), 3), #POR QUE ESSE 0.001???
-    print 'KB/s.'
+    print 'Vazão média', media(vazoes)
+    print 'Desvio padrão', desvioPadrao(vazoes)
 
+'''
+        print data
+        print 'Tamanho do pacote:', BUFSIZE
+        print '\nTempo entre geração de pacotes (segundos):', tempo
+        print '\nRaw timers:', t1, t2, t3, t4, t5
+        print '\nIntervalos:', t2-t1, t3-t2, t4-t3, t5-t4
+        print '\nTotal:', t5-t1
+        print '\nThroughput:', round((BUFSIZE*count*0.001) / (t5-t1), 3), #POR QUE ESSE 0.001???
+        print 'KB/s.'
+'''
 
 main()
